2037b646c3de9c39d02783b78f8d042c522ef507
itbideshi <itbideshi@gmail.com>
2016-12-22 19:13:17 +0530

    Add files via upload
    Files Added first time in project

---
+++ source/js/flexi_JS.js
@@ -0,0 +1,5 @@
+require('../images/flexi_logo.png');
+require('../images/id_card.png');
+require('../images/over18.png');
+require('../images/upfront_fee.png');
+require('../sass/flexi_style.scss');
