adddf26fb6b0219204388ea8280404653dc70799
itbideshi <itbideshi@gmail.com>
2016-12-23 13:25:29 +0530

    Adding comment in page RadioGroup paragraph changed to List

--- assets/js/flexi_JS.js
+++ assets/js/flexi_JS.js
@@ -1,1 +1,85 @@
-!function(n){function e(t){if(o[t])return o[t].exports;var r=o[t]={exports:{},id:t,loaded:!1};return n[t].call(r.exports,r,r.exports,e),r.loaded=!0,r.exports}var o={};return e.m=n,e.c=o,e.p="/assets/",e(0)}([function(n,e,o){o(2),o(3),o(4),o(5),o(1)},function(n,e){},function(n,e){n.exports="../images/flexi_logo.png"},function(n,e){n.exports="../images/id_card.png"},function(n,e){n.exports="../images/over18.png"},function(n,e){n.exports="../images/upfront_fee.png"}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/assets/";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	__webpack_require__(1);
+	__webpack_require__(2);
+	__webpack_require__(3);
+	__webpack_require__(4);
+	__webpack_require__(5);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = "../images/flexi_logo.png";
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = "../images/id_card.png";
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = "../images/over18.png";
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = "../images/upfront_fee.png";
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ }
+/******/ ]);
